708dd5dfb900c22a1c9de96d9069f74fb69e0a7e
BlueCastle2022 <alireza.adabi78@gmail.com>
2023-08-08 12:55:18 +0330

    add: vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Navbar/>
-    <Component {...pageProps} />
+      <Navbar />
+      <Component {...pageProps} />
+      <Analytics />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
